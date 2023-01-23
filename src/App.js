2136461aeb59797663f40a10ea99137df1b5b56c
ariviscomi <arielviscomi.mail@gmail.com>
2023-01-23 10:10:43 -0300

    Segunda Pre entrega

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Header } from './components/header/Header';
 import ItemListContainer from './components/itemListContainer/ItemListContainer';
 import {BrowserRouter, Routes,Route} from 'react-router-dom'
+import ItemDetailContainer from './components/itemDetailContainer/ItemDetailContainer';
 
 function App() {
 
@@ -10,7 +11,8 @@ function App() {
 		<BrowserRouter>
 			<Header/>
 			<Routes>
-				<Route path='/' element={<ItemListContainer/>}/>
+				<Route path='/' element={<ItemListContainer/>} />
+				<Route path='item' element={<ItemDetailContainer/>} />
 			</Routes>
 		</BrowserRouter>
 	); 
