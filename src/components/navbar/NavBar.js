0fd085c88b755d9028f106abe1a957bf5cda210a
ariviscomi <arielviscomi.mail@gmail.com>
2023-01-26 16:50:01 -0300

    ##

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,29 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
+
 const NavBar = () => {
 
 	return (
 		<nav className='headerNav'>
 			<ul>
+				<Box sx={{ minWidth: 120 }}>
+					<FormControl fullWidth>
+						<InputLabel id="select-label">Categoria</InputLabel>
+						<Select
+							labelId="select-label"
+							id="simple-select"
+							label="Categoria">
+							<MenuItem value={'all'}>Todos los Mates</MenuItem>
+							<MenuItem value={'natural'}>Natural Wood</MenuItem>
+							<MenuItem value={'minimal'}>Minimalista</MenuItem>
+							<MenuItem value={'tematico'}>Tematico</MenuItem>
+							<MenuItem value={'custom'}>Personalizar</MenuItem>
+						</Select>
+					</FormControl>
+				</Box>
 				<li>
-					<Link>Natural Wood</Link>
-				</li>
-				<li>
-					<Link>Minimalista</Link>
-				</li>
-				<li>
-					<Link>Tematicos</Link>
-				</li>
-				<li>
-					<Link>Personalizados</Link>
-				</li>
-				<li>
-					<input type='search' placeholder='Buscar'/> <span>Buscar</span>
+					<input type='search' placeholder='Buscar' /> <span>Buscar</span>
 				</li>
 			</ul>
 		</nav>
 	);
-};
+}
 
 export default NavBar;
